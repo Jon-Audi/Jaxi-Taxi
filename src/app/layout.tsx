3e85ba533376e54722b34b08df851a9134b16a2b
Jon Audiffred <jon.audiffred@gmail.com>
2025-06-29 03:26:50 +0000

    nope, i believe you are still going into "home/Jon/jaxi-taxi" when you s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Roboto:wght@700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
